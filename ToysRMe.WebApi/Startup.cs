27ae0512560d7f4a0361e28e9364d0d9d6ad1e5a
Javier Rivera <ingjrz@gmail.com>
2019-12-05 17:11:18 -0600

    New Angular project created to replace the old one made in Angular 2.0
    Upgraded CommonLibrary references.
    Upgraded WebApi references.
    Upgraded WebMVC references.
    
    Upgraded some WebNGular refrences but the project is no longer able to run properly
    Created WebAngular to replace old WebNGular project entirely.
    The new project seems very unstable, having lot of problems with dependencies, running npm audit fix to make it run.

--- ToysRMe.WebApi/Startup.cs
+++ ToysRMe.WebApi/Startup.cs
@@ -3,56 +3,56 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using ToysRMe.CommonLibrary;
 using ToysRMe.CommonLibrary.Contracts;
 using ToysRMe.CommonLibrary.Models;
 using ToysRMe.WebApi.Services;
 
 namespace ToysRMe.WebApi
 {
-  public class Startup
-  {
-    public Startup(IHostingEnvironment env)
+    public class Startup
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
-      if (env.IsEnvironment("Development"))
-      {
-        // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-      }
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+            }
 
-      builder.AddEnvironmentVariables();
-      Configuration = builder.Build();
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddTransient<IRepository<Product>, ProductRepository>();
+            services.AddCors(x => x.AddPolicy("Cors", builder =>
+            {
+                builder.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();                
+            }));
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseCors("Cors");
+            
+            app.UseMvc();
+        }
     }
-
-    public IConfigurationRoot Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container
-    public void ConfigureServices(IServiceCollection services)
-    {
-      // Add framework services.
-      services.AddTransient<IRepository<Product>, ProductRepository>();
-      services.AddCors(x => x.AddPolicy("Cors", builder => 
-      {
-        builder.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-      }));
-
-      services.AddMvc();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-    {
-      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-      loggerFactory.AddDebug();
-      app.UseCors("Cors");
-
-      app.UseMvc();
-    }
-  }
 }
